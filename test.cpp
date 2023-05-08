ca373e79ffa6456a72a80b24b0f253600dc0899a
Muhammad Hashim <113008076+MuhammadHashimRN@users.noreply.github.com>
2023-05-08 21:09:13 +0500

    test run

---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include<iostream>      // Header Files
+using namespace std;    // Used for naming variables and objects
+int main()              // The code in this is executed
+{                       // Starting bracket
+    cout << "hello world!";
+    return 0;           // Returns 0
+}                       // Ending bracket
