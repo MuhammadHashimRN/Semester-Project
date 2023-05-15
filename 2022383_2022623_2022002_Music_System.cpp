c80505f5e6b867b66bafbcc8992efaaa4fc24fe1
Muhammad Hashim <113008076+MuhammadHashimRN@users.noreply.github.com>
2023-05-15 23:48:10 +0500

    Edits

--- 2022383_2022623_2022002_Music_System.cpp
+++ 2022383_2022623_2022002_Music_System.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <Windows.h>  // Include Windows.h for PlaySound
+#include <Windows.h> // Include Windows.h for PlaySound
 
 #pragma comment(lib, "winmm.lib")
-class Song {
+class Song
+{
 public:
-    Song(std::string title, std::string artist) :
-        title_{ title }, artist_{ artist } {}
+    Song(std::string title, std::string artist) : title_{title}, artist_{artist} {}
 
-    std::string getTitle() const {
+    std::string getTitle() const
+    {
         return title_;
     }
 
-    std::string getArtist() const {
+    std::string getArtist() const
+    {
         return artist_;
     }
 
@@ -22,20 +24,25 @@ private:
     std::string artist_;
 };
 
-class Playlist {
+class Playlist
+{
 public:
     Playlist() = default; // Default constructor
 
-    Playlist(std::string name) : name_{ name } {}
+    Playlist(std::string name) : name_{name} {}
 
-    void addSong(const Song& song) {
+    void addSong(const Song &song)
+    {
         songs_.push_back(song);
         std::cout << "Added song: " << song.getTitle() << std::endl;
     }
 
-    void removeSong(const Song& song) {
-        for (auto it = songs_.begin(); it != songs_.end(); ++it) {
-            if (it->getTitle() == song.getTitle()) {
+    void removeSong(const Song &song)
+    {
+        for (auto it = songs_.begin(); it != songs_.end(); ++it)
+        {
+            if (it->getTitle() == song.getTitle())
+            {
                 songs_.erase(it);
                 std::cout << "Removed song: " << song.getTitle() << std::endl;
                 return;
@@ -44,25 +51,75 @@ public:
         std::cout << "Song not found in playlist." << std::endl;
     }
 
-    void printPlaylist() const {
+    void printPlaylist() const
+    {
         std::cout << "Playlist: " << std::endl;
-        for (int i = 0; i < songs_.size(); ++i) {
+        for (int i = 0; i < songs_.size(); ++i)
+        {
             std::cout << i << ": " << songs_[i].getTitle() << " by " << songs_[i].getArtist() << std::endl;
         }
     }
 
-    void play() const {
-        if (songs_.empty()) {
+    void play() const
+    {
+        if (songs_.empty())
+        {
             std::cout << "Playlist is empty." << std::endl;
             return;
         }
 
         // Print playlists
         std::cout << "Playlists: " << std::endl;
-        for (int i = 0; i < playlists_.size(); ++i) {
+        for (int i = 0; i < playlists_.size(); ++i)
+        {
             std::cout << i << ": " << playlists_[i].name_ << std::endl;
         }
 
+        nt playlistChoice;
+        std::cout << "Choose a playlist: ";
+        std::cin >> playlistChoice;
 
+        if (playlistChoice < 0 || playlistChoice >= playlists_.size())
+        {
+            std::cout << "Invalid playlist choice." << std::endl;
+            return;
+        }
 
+        const Playlist &selectedPlaylist = playlists_[playlistChoice];
+        selectedPlaylist.printPlaylist();
 
+        int songChoice;
+        std::cout << "Choose a song from the playlist: ";
+        std::cin >> songChoice;
+
+        if (songChoice < 0 || songChoice >= selectedPlaylist.songs_.size())
+        {
+            std::cout << "Invalid song choice." << std::endl;
+            return;
+        }
+
+        const Song &selectedSong = selectedPlaylist.songs_[songChoice];
+        std::cout << "Playing: " << selectedSong.getTitle() << std::endl;
+        PlaySound(TEXT("song.wav"), NULL, SND_FILENAME | SND_ASYNC);
+    }
+
+    void pause() const
+    {
+        PlaySound(NULL, NULL, SND_ASYNC); // Stop the playback
+        std::cout << "Pausing the song." << std::endl;
+    }
+
+    void changeVolume(int volume)
+    {
+        // Add your implementation to adjust the volume
+        std::cout << "Changing volume to: " << volume << std::endl;
+    }
+
+    static std::vector<Playlist> playlists_;
+
+private:
+    std::string name_;
+    std::vector<Song> songs_;
+};
+
+std::vector<Playlist> Playlist::playlists_;
