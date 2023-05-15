a81c9ec50c3be4d501fa22c7c15b7a8bc7bd74d7
Muhammad Hashim <113008076+MuhammadHashimRN@users.noreply.github.com>
2023-05-15 23:16:07 +0500

    Classes

--- test.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>      // Header Files
-using namespace std;    // Used for naming variables and objects
-int main()              // The code in this is executed
-{                       // Starting bracket
-    cout << "hello world!";
-    return 0;           // Returns 0
-}                       // Ending bracket
